8e0df63c8ed38d319a08afca85450ef358380012
hasanbdmehadi <57194633+hasanbdmehadi@users.noreply.github.com>
2023-12-21 05:03:45 +0600

    Gateway done

---
+++ AttendanceV5/AttendanceV5/Gateway/FacultyGateway.cs
@@ -0,0 +1,236 @@
+﻿using AttendanceV5.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceV5.Gateway
+{
+    class FacultyGateway
+    {
+        string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        public bool CheckHas(Faculty faculty)
+        {
+            string s = faculty.id;
+            SqlConnection con = new SqlConnection(cs);
+            string q = "select * from Faculty where Id = '" + s + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+            }
+        }
+
+        public bool CheckShortNameHas(Faculty faculty)
+        {
+            string s = faculty.shortName;
+            SqlConnection con = new SqlConnection(cs);
+            string q = "select * from Faculty where ShortName = '" + s + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+            }
+        }
+
+        public string Insert(Faculty faculty)
+        {
+            bool chk = CheckHas(faculty);
+            string flag = "";
+            if (chk)
+            {
+                flag = "This information is already exist";
+            }
+            else
+            {
+                bool chksn = CheckShortNameHas(faculty);
+                if (chksn)
+                {
+                    flag = "This Short Name is already taken. Try Another.";
+                }
+                else
+                {
+                    //Faculty Table insertion
+                    SqlConnection con = new SqlConnection(cs);
+                    string q = "insert into Faculty (Id, Name, ShortName, Password, Department, Pgdit, Pmit, Msc, Bsc, Phone) values ('" + faculty.id + "','" + faculty.name + "','" + faculty.shortName + "','" + faculty.password + "','" + faculty.department + "','" + faculty.Pgdit + "','" + faculty.Pmit + "','" + faculty.Msc + "','" + faculty.Bsc + "','"+faculty.phone+"')";
+                    SqlCommand cmd = new SqlCommand(q, con);
+                    con.Open();
+                    int flag1 = cmd.ExecuteNonQuery();
+
+                    if (flag1 > 0) flag = "Faculty Information is Saved";
+                    else flag = "Faculty Information is not Saved";
+                }
+
+
+            }
+
+            return flag;
+
+        }
+
+        public bool Update(Faculty faculty)
+        {
+
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "UPDATE Faculty SET Name = '" + faculty.name + "', ShortName = '" + faculty.shortName + "', Password = '" + faculty.password + "', Pgdit = '" + faculty.Pgdit + "', Pmit = '" + faculty.Pmit + "', Msc = '" + faculty.Msc + "', Bsc = '" + faculty.Bsc + "' Where Id = '" + faculty.id + "' ";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+
+        public bool Delete(string fId)
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "DELETE FROM Faculty WHERE Id = '" + fId + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+
+        public bool Clear()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "DELETE FROM Faculty";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+
+        public List<string> GetFacultyIdList()
+        {
+            List<string> list = new List<string>();
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "SELECT Id FROM Faculty";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                string s = "";
+                while (reader.Read())
+                {
+                    s = reader["Id"].ToString();
+                    list.Add(s);
+                }
+            }
+
+            return list;
+        }
+
+
+
+        public List<string> GetFacultyId()
+        {
+            List<string> list = new List<string>();
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "SELECT Id FROM Faculty";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                string s = "";
+                while (reader.Read())
+                {
+                    s = reader["Id"].ToString();
+                    list.Add(s);
+                }
+            }
+
+            return list;
+        }
+
+        public string GetFacultyProgramById(string id)
+        {
+            string programs = "";
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "SELECT ProgramName FROM ProgramFaculty WHERE FId = '" + id + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+
+                string s = "";
+                while (reader.Read())
+                {
+                    s = reader["ProgramName"].ToString();
+                    programs += s;
+                    //Console.WriteLine(programs);
+
+                }
+            }
+
+            return programs;
+        }
+
+        public List<string> GetFacultyInfoWithoutId(string id)
+        {
+            List<string> list = new List<string>();
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "SELECT * FROM Faculty WHERE Id = '" + id + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                string s = "";
+                while (reader.Read())
+                {
+                    s = reader["Name"].ToString();
+                    list.Add(s);
+                    s = reader["ShortName"].ToString();
+                    list.Add(s);
+                    s = reader["Password"].ToString();
+                    list.Add(s);
+                    s = reader["Department"].ToString();
+                    list.Add(s);
+                    //Console.WriteLine(s);
+                    s = GetFacultyProgramById(id);
+                    list.Add(s);
+                }
+            }
+
+
+            return list;
+        }
+    }
+}
---
+++ AttendanceV5/AttendanceV5/Gateway/StudentGateway.cs
@@ -0,0 +1,99 @@
+﻿using AttendanceV5.UI;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttendanceV5.Gateway
+{
+    class StudentGateway
+    {
+        string cs = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+
+        public bool CheckHas(Student student)
+        {
+            int id = student.StudentId;
+            SqlConnection con = new SqlConnection(cs);
+            string q = "select * from Student where StudentId = " + id + "";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                con.Close();
+                return true;
+            }
+            else
+            {
+                con.Close();
+                return false;
+            }
+        }
+
+
+
+        public string Insert(Student student)
+        {
+            string flag = "";
+            if (CheckHas(student))
+            {
+                flag = "This Student is already exist";
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(cs);
+                string q = "insert into Student (StudentId, StudentName, StudentAddress, StudentDepartment, StudentSemester, StudentProgram, GuardianPhone) values (" + student.StudentId + ",'" + student.StudentName + "', '" + student.StudentAddress + "','" + student.StudentDepartment + "','" + student.StudentSemester + "','" + student.StudentProgram + "','"+student.GuardianPhone+"')";
+                SqlCommand cmd = new SqlCommand(q, con);
+                con.Open();
+                int reader = cmd.ExecuteNonQuery();
+                if (reader > 0) flag = "Information is saved";
+                else flag = "Couldn't save";
+            }
+            return flag;
+        }
+
+        public bool Delete(string sId)
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "DELETE FROM Student WHERE StudentId = '" + sId + "'";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+
+        public bool Update(Student student)
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "UPDATE Student SET StudentName = '" + student.StudentName + "', StudentAddress = '" + student.StudentAddress + "' ,StudentDepartment = '" + student.StudentDepartment + "', StudentSemester = '" + student.StudentSemester + "',StudentProgram = '" + student.StudentProgram + "',GuardianPhone = '"+student.GuardianPhone+"' Where StudentId = " + student.StudentId + " ";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+
+        public bool Clear()
+        {
+
+            SqlConnection con = new SqlConnection(cs);
+            string q = "DELETE FROM Student";
+            SqlCommand cmd = new SqlCommand(q, con);
+            con.Open();
+            int rowAffected = cmd.ExecuteNonQuery();
+            if (rowAffected > 0) return true;
+            else return false;
+
+        }
+    }
+}
